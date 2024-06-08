2d15053909341da4f3d9f5f2a7fe1ff0b15d23b6
Yevhenns <gifestum@gmail.com>
2024-06-08 14:55:05 +0300

    styles fix

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,7 +6,7 @@ export type IconProps = {
 
 export const Icon = ({ iconId }: IconProps) => {
   return (
-    <svg width={20} height={20}>
+    <svg width={20} height={20} fill="white">
       <use href={`/icons/sprite.svg#${iconId}`} />
     </svg>
   );
--- src/containers/SearchCoinsList/SearchCoinsList.tsx
+++ src/containers/SearchCoinsList/SearchCoinsList.tsx
@@ -52,7 +52,7 @@ export const SearchCoinsList = ({
                     onClick={() => addToFavoriteCoins(filteredCoin)}
                   />
                 )}
-                {filteredCoin}
+                <span>{filteredCoin}</span>
               </div>
             );
           })
